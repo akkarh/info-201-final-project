9eb9b6d7d52dcadc0b2b6b35228a5004ee32794b
Nathaniel Aune <naune15@gmail.com>
2017-03-01 14:08:54 -0800

    added widgets for preferences

--- ui.R
+++ ui.R
@@ -1,10 +1,15 @@
 library("shiny")
 
 ui <- fluidPage(
-  titlePanel("Title"),
+  titlePanel("Global Melody"),
   sidebarLayout(
     sidebarPanel(
-      textInput('region', label = h3("Region"), value = "Enter text...")
+      textInput('country', label = h3("Country"), value = "Enter text..."),
+      sliderInput('popularity', label = h3("Popularity (100 being most popular)"), min = 0, max = 100, value = 50),
+      sliderInput('loudness', label = h3("Loudness (dB)"), min = -60, max = 0, value = -30),
+      sliderInput('danceability', label = h3("Danceability"), min = 0.0, max = 1.0, value = 0.5),
+      sliderInput('energy', label = h3("Energy"), min = 0.0, max = 1.0, value = 0.5),
+      sliderInput('instrumentalness', label = h3("Instrumentalness (it's a thing!)"), min = 0.0, max = 1.0, value = 0.5)
     ), 
     mainPanel(
       tabsetPanel(type = "tabs", 
