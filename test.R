331ba23adbd4c881ddc18b97c92f065e6abd3ee0
charlyecastro <castroc@uw.edu>
2017-03-02 13:36:22 -0800

    Added correct test file hahahaha

--- test.R
+++ test.R
@@ -5,12 +5,21 @@ library("ggplot2")
 
 source("apikey.R")
 
-#base.uri <- "https://api.spotify.com/v1"
-#resource <- "/recommendations"
-#query.params <- list("api-key"=nyt.apikey, query=movie)
+
+response = POST(
+  'https://accounts.spotify.com/api/token',
+  accept_json(),
+  authenticate(client.id, client.secret),
+  body = list(grant_type = 'client_credentials'),
+  encode = 'form',
+  verbose()
+)
+mytoken = content(response)$access_token
+HeaderValue = paste0('Bearer ', mytoken)
 
 
-response <- GET("https://api.spotify.com/v1/audio-analysis/3JIxjvbbDrA9ztYlNcp3yL",api.key )
-body <- fromJSON(content(response, "text"))
 
-View(body)
+URI = "https://api.spotify.com/v1/search?q=popularity=100&type=track&market=US"
+response2 = GET(url = URI, add_headers(Authorization = HeaderValue))
+rec = content(response2)
+print(rec)
