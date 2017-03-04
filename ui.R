24eedaa9ce861cf52d358ff71201b020b4ef379e
Nathaniel Aune <naune15@gmail.com>
2017-03-04 15:40:47 -0800

    completed ui side of app

--- ui.R
+++ ui.R
@@ -1,21 +1,14 @@
 library("shiny")
 
 ui <- fluidPage(
-  titlePanel("Global Melody"),
+  titlePanel("Crime Rates in Seattle"),
   sidebarLayout(
     sidebarPanel(
-      textInput('country', label = h3("Country"), value = "Enter text..."),
-      sliderInput('popularity', label = h3("Popularity (100 being most popular)"), min = 0, max = 100, value = 50),
-      sliderInput('loudness', label = h3("Loudness (dB)"), min = -60, max = 0, value = -30),
-      sliderInput('danceability', label = h3("Danceability"), min = 0.0, max = 1.0, value = 0.5),
-      sliderInput('energy', label = h3("Energy"), min = 0.0, max = 1.0, value = 0.5),
-      sliderInput('instrumentalness', label = h3("Instrumentalness (it's a thing!)"), min = 0.0, max = 1.0, value = 0.5)
+      radioButtons('xaxis', "X-Axis", choices = c("Years", "Months", "Crime Type")),
+      conditionalPanel(condition = "input.xaxis === 'Months'", selectInput('year', "Year", choices = c(2008:2017)))
     ), 
     mainPanel(
-      tabsetPanel(type = "tabs", 
-                  tabPanel("Tab 1"),
-                  tabPanel("Tab 2")
-      )
+      
     )
   )
 )
