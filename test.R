3037afde7478f11fca539b796f74d14202b21540
charlyecastro <castroc@uw.edu>
2017-03-04 14:58:15 -0800

    replaced sector with precinct

--- test.R
+++ test.R
@@ -6,12 +6,12 @@ library("ggplot2")
 
 # Read in CSV file
 seattle <- read.csv('data/Seattle_Crime_Stats_2008_To_Present.csv', stringsAsFactors = FALSE)
-
+View(seattle)
 #created a year column
 seattle.crime <- mutate(seattle, year = substr(REPORT_DATE, 7, 10)) %>% 
   mutate(month = substr(REPORT_DATE, 1, 2)) %>% 
-  select(CRIME_TYPE, STAT_VALUE, REPORT_DATE, Sector, month, year)
-names(seattle.crime) <- c("crime.type", "stat.value", "report.date", "sector", "month", "year")
+  select(CRIME_TYPE, STAT_VALUE, REPORT_DATE, Precinct, month, year)
+names(seattle.crime) <- c("crime.type", "stat.value", "report.date", "precinct", "month", "year")
 View(seattle.crime)
 
 colnames(seattle.crime)
