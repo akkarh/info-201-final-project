a2fec90122e63cc8ccef2a662d16790ad0162a5c
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2017-03-05 21:18:54 -0800

    Add visualizations

--- test.R
+++ test.R
@@ -29,3 +29,33 @@ group_by(precinct, crime.type) %>% summarise(count = sum(stat.value))
 View(crime.precincts)
 
 
+# Crime v. Years Plot
+# If we are going to have an option to filter the data by crime type, we need the following line of code:
+  # crime.years <- crime.years %>% filter(crime.type == "crime type")
+ggplot(crime.years, aes(x = year, y = count, fill = crime.type)) +
+  geom_col() +
+  labs(x = "Years", y = "Number of Occurences of the Crime", title = "Frequencies of Crime Types Over Time") +
+  scale_fill_brewer(type = "qual", palette = "Set2")
+  
+
+# Crime v. Months
+# Month numbers to month names: to make the graph more readable
+months <- c("01" = "Jan", "02" = "Feb", "03" = "Mar", "04" = "Apr",
+               "05" = "May", "06" = "Jun", "07" = "Jul", "08" = "Aug",
+               "09" = "Sep", "10" = "Oct", "11" = "Nov", "12" = "Dec")
+months.as.numbers <- crime.months$month
+months.names <- months[months.as.numbers]
+crime.months$month <- months.names
+# Prevents alphabetical sorting of the x axis.
+crime.months$month <- factor(crime.months$month, levels = unique(crime.months$month))
+ggplot(crime.months, aes(x = month, y = count, fill = crime.type)) +
+  scale_x_discrete("month") +
+  geom_col() +
+  labs(x = "Months", y = "Number of occurences of the Crime", title = "Frequencies of Crime Types in *2008*") +
+  scale_fill_brewer(type = "qual", palette = "Set2")
+
+# Crime v. Precincts
+ggplot(crime.precincts, aes(x = precinct, y = count, fill = crime.type)) +
+  geom_col() +
+  labs(x = "Precincts", y = "Number of occurences of the Crime", title = "Frequencies of Crime Types by Precincts") +
+  scale_fill_brewer(type = "qual", palette = "Set2")
